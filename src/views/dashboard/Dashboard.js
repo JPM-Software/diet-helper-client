23888e731c52529560fa8b0cf7db12e79ab0f644
Marek Szymański <marek.sz026@gmail.com>
2021-01-11 23:22:20 +0100

    change calendar view, add inputs, add form and get user details to view

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -6,23 +6,27 @@ import {
   CCol,
   CJumbotron,
   CRow,
-} from '@coreui/react'
-import { useHistory } from 'react-router-dom';
+} from "@coreui/react";
+import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
   const history = useHistory();
-  const goToDiary = () => history.push('/diary');
-  const goToCalculator = () => history.push('/calculator');
+  const goToDiary = () => history.push("/diary");
+  const goToCalculator = () => history.push("/calculator");
   return (
     <>
       <CRow>
-        <CCol>
+        <CCol xs="12" md="6">
           <CCard>
             <CCardBody>
               <CJumbotron className="border">
-                <h1 className="display-4">Kalkulator zapotrzebowania kalorycznego</h1>
+                <h1 className="display-4">
+                  Kalkulator zapotrzebowania kalorycznego
+                </h1>
                 <p className="lead">
-                  Dowiedz się podstawowych informacji o swojej wadze i zapotrzebowaniu kalorycznym oraz wybierz cel jaki chcesz osiągnąć.
+                  Dowiedz się podstawowych informacji o swojej wadze i
+                  zapotrzebowaniu kalorycznym oraz wybierz cel jaki chcesz
+                  osiągnąć.
                 </p>
                 <hr className="my-2" />
                 <p className="lead">
@@ -34,13 +38,13 @@ const Dashboard = () => {
             </CCardBody>
           </CCard>
         </CCol>
-        <CCol>
+        <CCol xs="12" md="6">
           <CCard>
             <CCardBody>
               <CJumbotron className="border">
-              <h1 className="display-4">Dziennik twojego spożycia</h1>
+                <h1 className="display-4">Dziennik twojego spożycia</h1>
                 <p className="lead">
-                  Zapisuj i kontroluj swoje dzienne spożycie. 
+                  Zapisuj i kontroluj swoje dzienne spożycie.
                 </p>
                 <hr className="my-2" />
                 <p className="lead">
