1498a021d8e9e37b83aa71e6f72ecb79eeb5df9a
Piotr Niemczyk <niemczykpiotr13@gmail.com>
2021-01-13 02:05:06 +0100

    change register layout

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -3,6 +3,7 @@ import {
   CButton,
   CCard,
   CCardBody,
+  CCardGroup,
   CCol,
   CContainer,
   CInput,
@@ -12,7 +13,7 @@ import {
   CRow,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { Form, Formik } from "formik";
 
 const Register = () => {
@@ -63,60 +64,86 @@ const Register = () => {
     <div className="c-app c-default-layout flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md="9" lg="7" xl="6">
-            <CCard className="mx-4">
-              <CCardBody className="p-4">
-                <Formik initialValues={formData} onSubmit={sendData}>
-                  {({ setFieldValue }) => {
-                    return (
-                      <Form>
-                        <h1>Rejestracja</h1>
-                        <p className="text-muted">Utwórz swoje konto</p>
-                        <CInputGroup className="mb-3">
-                          <CInputGroupPrepend>
-                            <CInputGroupText>
-                              <CIcon name="cil-user" />
-                            </CInputGroupText>
-                          </CInputGroupPrepend>
-                          <CInput
-                            id="login"
-                            name="login"
-                            type="text"
-                            placeholder="Nazwa"
-                            autoComplete="username"
-                            required
-                            onChange={({ target }) =>
-                              setFieldValue("login", target.value)
-                            }
-                          />
-                        </CInputGroup>
-                        <CInputGroup className="mb-3">
-                          <CInputGroupPrepend>
-                            <CInputGroupText>
-                              <CIcon name="cil-lock-locked" />
-                            </CInputGroupText>
-                          </CInputGroupPrepend>
-                          <CInput
-                            id="password"
-                            name="password"
-                            type="password"
-                            placeholder="Hasło"
-                            autoComplete="new-password"
-                            required
-                            onChange={({ target }) =>
-                              setFieldValue("password", target.value)
-                            }
-                          />
-                        </CInputGroup>
-                        <CButton type="submit" color="success" block>
-                          Utwórz konto
-                        </CButton>
-                      </Form>
-                    );
-                  }}
-                </Formik>
-              </CCardBody>
-            </CCard>
+          <CCol md="8">
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <Formik initialValues={formData} onSubmit={sendData}>
+                    {({ setFieldValue }) => {
+                      return (
+                        <Form>
+                          <h1>Rejestracja</h1>
+                          <p className="text-muted">Utwórz swoje konto</p>
+                          <CInputGroup className="mb-3">
+                            <CInputGroupPrepend>
+                              <CInputGroupText>
+                                <CIcon name="cil-user" />
+                              </CInputGroupText>
+                            </CInputGroupPrepend>
+                            <CInput
+                              id="login"
+                              name="login"
+                              type="text"
+                              placeholder="Nazwa"
+                              autoComplete="username"
+                              required
+                              onChange={({ target }) =>
+                                setFieldValue("login", target.value)
+                              }
+                            />
+                          </CInputGroup>
+                          <CInputGroup className="mb-3">
+                            <CInputGroupPrepend>
+                              <CInputGroupText>
+                                <CIcon name="cil-lock-locked" />
+                              </CInputGroupText>
+                            </CInputGroupPrepend>
+                            <CInput
+                              id="password"
+                              name="password"
+                              type="password"
+                              placeholder="Hasło"
+                              autoComplete="new-password"
+                              required
+                              onChange={({ target }) =>
+                                setFieldValue("password", target.value)
+                              }
+                            />
+                          </CInputGroup>
+                          <CButton type="submit" color="success" block>
+                            Utwórz konto
+                          </CButton>
+                        </Form>
+                      );
+                    }}
+                  </Formik>
+                </CCardBody>
+              </CCard>
+              <CCard
+                className="text-white bg-gradient-info py-5 d-md-down-none"
+                style={{ width: "44%" }}
+              >
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Logowanie</h2>
+                    <p>
+                      Jeśli posiadasz już konto, kliknij przyciks poniżej i
+                      przejdź do logowania.
+                    </p>
+                    <Link to="/login">
+                      <CButton
+                        color="primary"
+                        className="mt-3"
+                        active
+                        tabIndex={-1}
+                      >
+                        Zaloguj się!
+                      </CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
           </CCol>
         </CRow>
       </CContainer>
